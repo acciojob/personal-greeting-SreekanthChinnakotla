cb46cf01abcb19c125042ff3a4445f58f4d407ff
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2024-01-02 12:36:13 +0000

    check

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,22 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState('');
+  
+  const updateGreeting = (inputName) => {
+    setName(inputName);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Enter your name:</h1>
+       
+      <label >Enter your name:</label>
+      <input
+        type="text"
+        id="nameInput"
+        onChange={(e) => updateGreeting(e.target.value)}
+      />
+      <h1>Hello {name}!</h1>  
     </div>
   )
 }
